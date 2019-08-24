3277a299f4ada036be3ef58171b368e5d12c7cb8
ilabstaff <ilabstaff@172.16.116.68>
2019-08-24 10:54:55 -0700

    remarkable

---
+++ src/_03_morning_zombie/remarkable.java
@@ -0,0 +1,27 @@
+package _03_morning_zombie;
+
+import javax.swing.JOptionPane;
+
+public class remarkable {
+
+	
+public static void main(String[] args) {
+String maristar="has wavy hair";
+String PinkFact="is tall";
+String jessica="has brown eyes";
+String input= JOptionPane.showInputDialog("enter a name");
+if (input.equals("Mariene")) {
+JOptionPane.showMessageDialog(null,"Mariene name"+maristar);
+}
+if (input.equals("PinkFact")) {
+JOptonp
+}
+}
+
+}	
+	
+	
+	
+	
+	
+}
